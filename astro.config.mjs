1f532021290498ce935cdcbdd24ddd114a271482
JorgeEscalonas <escalonajorge73@gmail.com>
2025-09-08 21:13:27 -0400

    optimized images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
-import vercel from '@astrojs/vercel/static'; // Adaptador para modo estático
+import vercel from '@astrojs/vercel';
+
 
 // https://astro.build/config
 export default defineConfig({
