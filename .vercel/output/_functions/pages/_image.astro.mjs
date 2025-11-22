4169bd93d1af5a67fa371d106783feeab58284dc
JorgeEscalonas <escalonajorge73@gmail.com>
2025-11-21 23:43:43 -0400

    optimizacion del SEO

---
+++ .vercel/output/_functions/pages/_image.astro.mjs
@@ -0,0 +1,2 @@
+export { a as page } from '../chunks/generic_Bh3Xli3F.mjs';
+export { renderers } from '../renderers.mjs';
--- astro.config.mjs
+++ astro.config.mjs
@@ -5,15 +5,16 @@ import vercel from '@astrojs/vercel';
 import { fileURLToPath } from 'url';
 import path from 'path';
 
+import sitemap from '@astrojs/sitemap';
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    preact({
-      compat: true
-    })
-  ],
+  integrations: [preact({
+    compat: true
+  }), sitemap()],
+  site: 'https://jorgeescalona.vercel.app/',
   output: 'server',
   adapter: vercel({
     webAnalytics: {
